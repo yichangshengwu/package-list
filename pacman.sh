f65fb1e03e193fc4180fbc0df119a4e86a71f4b4
yichangshengwu <x@localhost.localdomain>
2020-08-18 13:48:20 +0800

    update

---
+++ pacman.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+## A
+pacman -Sy acpi acpi_call ark audacity bbswitch bluedevil breeze breeze-gtk breeze-icons btfs btfs-progs code copyq cuetools deadbeef desktop-file-utils dina-font dkms dosfstools e2fsprogs exfat-utils fakeroot fcitx5-im fcitx5-rime firefox-developer-edition-i18n-zh-cn flameshot gedit git gnome-calculator gnome-disk-utility gpart gparted grub-theme-vimix gsmartcontrol gvfs-mtp intel-gpu-tools intel-media-driver kdeconnect kde-gtk-config keepassxc kgpg kid3 konsole ksysguard kwalletmanager lightdm-gtk-greeter-settings man-pages-zh_cn mesa-vdpau mtpfs nano neofetch ntfs-3g nvidia nvidia-settings nvidia-utils onboard opencl-nvidia p7zip peek powerdevil qt5ct squashfs-tools squashfuse systemsettings telegram-desktop terminator thunar udftools unzip v2ray-domain-list-community vlc wqy-microhei wqy-zenhei zsh zsh-autosuggestions zsh-completions zshdb zsh-doc zsh-history-substring-search zsh-syntax-highlighting
+
+## B
+# pacman -S audacious audacious-plugins chromium kcron kompare kpatience nvtop obs-studio rxvt-unicode spectacle virtualbox-host-dkms 
